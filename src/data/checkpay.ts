9135775fad9c4b1f1659615be6f33869a769e496
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-09-18 18:33:35 -0300

    fix: url projects

--- src/data/checkpay.ts
+++ src/data/checkpay.ts
@@ -1,14 +1,15 @@
+const url = process.env.NEXT_PUBLIC_PATH_IMAGE;
 export const imagesCheckPay = {
-  thumb: '/portfolio/checkpay/check-1.png',
+  thumb: `${url}/portfolio/checkpay/check-1.png`,
   images: [
-    '/portfolio/checkpay/check-1.png',
-    '/portfolio/checkpay/check-2.png',
-    '/portfolio/checkpay/check-3.png',
-    '/portfolio/checkpay/check-4.png',
-    '/portfolio/checkpay/check-5.png',
-    '/portfolio/checkpay/check-6.png',
-    '/portfolio/checkpay/check-7.png',
-    '/portfolio/checkpay/check-8.png',
-    '/portfolio/checkpay/check-9.png',
+    `${url}/portfolio/checkpay/check-1.png`,
+    `${url}/portfolio/checkpay/check-2.png`,
+    `${url}/portfolio/checkpay/check-3.png`,
+    `${url}/portfolio/checkpay/check-4.png`,
+    `${url}/portfolio/checkpay/check-5.png`,
+    `${url}/portfolio/checkpay/check-6.png`,
+    `${url}/portfolio/checkpay/check-7.png`,
+    `${url}/portfolio/checkpay/check-8.png`,
+    `${url}/portfolio/checkpay/check-9.png`,
   ],
 };
